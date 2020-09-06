411abb21f32f968db803e4229c000359cd688670
Unknown <arthur.delamare1@viacesi.fr>
2020-09-06 16:20:10 +0200

    test(core): add match test

---
+++ test/match.test.js
@@ -0,0 +1,7 @@
+const recognizy = require ('../dist/recognizy');
+
+test("find a match into list", () => {
+  const array = ["I like apples", "tomatoes are great", "pizzas are the best"];
+  const searched = "apples";
+  expect(recognizy.match(searched, array)).toBe("I like apples");
+});
