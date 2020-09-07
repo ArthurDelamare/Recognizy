30a0860f0ca00d3db70d5b425f3cebead704248b
Unknown <arthur.delamare1@viacesi.fr>
2020-09-07 15:15:29 +0200

    test(core): test jacard distance function

--- test/match.test.js
+++ test/match.test.js
@@ -5,3 +5,10 @@ test("find a match into list", () => {
   const searched = "apples";
   expect(recognizy.match(searched, array)).toBe("I like apples");
 });
+
+test("jaccard", () => {
+  const sentence = "Jest is a nice testing library";
+  const array = sentence.split(" ");
+  const searched = ["testing", "library"];
+  expect(recognizy.jaccard(new Set(array), new Set(searched))).toBe(1 - (2/6));
+});
