575c781ff56265c1a4aa5182cbde47cc95f6010f
ArthurD <arthur.delamare1@viacesi.fr>
2020-09-11 16:39:35 +0200

    test(core): check that an error is thrown if no keys and object used

--- test/fuzzy.test.js
+++ test/fuzzy.test.js
@@ -9,7 +9,7 @@ test("When creating a Fuzzy instance, items list should be accessible", () => {
     "Donut jelly cake I love",
   ];
   const fuzzy = new Fuzzy(items);
-  
+
   expect(fuzzy.items).not.toBe(undefined);
   expect(fuzzy.items).not.toBe(null);
   expect(fuzzy.items).toBe(items);
@@ -46,4 +46,27 @@ test("Should return several matches", () => {
     index: 0,
     matchesAt: [ 43 ]
   });
+});
+
+test("Should throw an error when using object without specifying keys in options", () => {
+  const items = [
+    {
+      movie: "Tenet",
+      synopsis: "Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
+      author: "Christopher Nolan"
+    },
+    {
+      movie: "Inception",
+      synopsis: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+      author: "Christopher Nolan"
+    },
+    {
+      movie: "The perks of being a wallflower",
+      synopsis: "An introvert freshman is taken under the wings of two seniors who welcome him to the real world.",
+      author: "Stephen Chbosky"
+    }
+  ];
+  const fuzzy = new Fuzzy(items);
+
+  expect(() => { fuzzy.search("wallflower") }).toThrowError(new Error("Used object without specifying keys in options."));
 });
