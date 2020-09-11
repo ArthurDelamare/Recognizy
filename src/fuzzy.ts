3112a22cbd4c2087ed058987357d4cfbf67203e4
ArthurD <arthur.delamare1@viacesi.fr>
2020-09-11 12:28:27 +0200

    feat(core): add stringSearch and search

--- src/fuzzy.ts
+++ src/fuzzy.ts
@@ -3,19 +3,41 @@ import { rabinkarp } from "./hash/rabinkarp";
 export class Fuzzy {
   public readonly items: any[] = [];
 
-
   constructor(items: any[] ) {
     this.items = items;
   }
 
   public search(pattern: string, options: FuzzyOptions) {
+    let matches = [];
     
+    if (this.items.every(item => typeof(item) === "string")) {
+      matches = this.stringSearch(pattern);
+    }
+
+    return matches;
+  }
+
+  private stringSearch(pattern: string) {
+    const matches = [];
+
+    for (const [index, item] of this.items.entries()) {
+      const result = rabinkarp(item, pattern);
+      if (result.length > 0) {
+        matches.push({
+          item: item,
+          index: index,
+          matchesAt: result
+        });
+      }
+    }
+
+    return matches;
   }
 }
 
 export interface FuzzyOptions {
   caseSensitive?: boolean;
-  keys?: string[]; 
+  keys?: string[];
 }
 
 export interface FuzzyResult<T> {
