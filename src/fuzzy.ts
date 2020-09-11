ba65f9b3d8beb7baa1892b4f683e1735d436beae
ArthurD <arthur.delamare1@viacesi.fr>
2020-09-11 18:45:50 +0200

    feat(core): add objectSearch method

--- src/fuzzy.ts
+++ src/fuzzy.ts
@@ -14,13 +14,15 @@ export class Fuzzy {
       matches = this.stringSearch(pattern);
     } else if (options.keys == null) {
       throw new Error("Used object without specifying keys in options.");
+    } else {
+      matches = this.objectSearch(pattern, options.keys);
     }
 
     return matches;
   }
 
   private stringSearch(pattern: string) {
-    const matches = [];
+    const matches: FuzzyResult[] = [];
 
     for (const [index, item] of this.items.entries()) {
       const result = rabinkarp(item, pattern);
@@ -35,6 +37,55 @@ export class Fuzzy {
 
     return matches;
   }
+
+  private objectSearch(pattern: string, keys: string[]) {
+    const matches: FuzzyResult[] = [];
+
+    for (const [index, item] of this.items.entries()) {
+      const result = this.itemSearch(pattern, item, keys);
+
+      if (result.item != null) {
+        result.index = index;
+        matches.push(result);
+      }
+    }
+
+    
+
+    return matches;
+  }
+
+  private itemSearch(pattern: string, item: any, keys: string[]): FuzzyResult {
+    const match: FuzzyResult = {};
+
+    for (const key in keys) {
+      const variable = this.items[key];
+
+      if (typeof(variable) === "string") {
+        const result = rabinkarp(variable, pattern);
+
+        if (result.length > 0) {
+          match.item = item;
+          return match;
+        }
+      }
+
+      if (Array.isArray(variable)) {
+        for (const value in variable) {
+          if (typeof(value) === "string") {
+            const result = rabinkarp(value, pattern);
+
+            if (result.length > 0) {
+              match.item = item;
+              return match;
+            }
+          }
+        }
+      }
+    }
+
+    return match;
+  }
 }
 
 export interface FuzzyOptions {
@@ -42,8 +93,8 @@ export interface FuzzyOptions {
   keys?: string[];
 }
 
-export interface FuzzyResult<T> {
-  item: T,
-  index: number,
-  matchesAt: number[]
+export interface FuzzyResult {
+  item?: any,
+  index?: number,
+  matchesAt?: number[]
 }
