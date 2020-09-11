4871bb361bde36e6be7ab00fc65bfb3bdd61b04b
ArthurD <arthur.delamare1@viacesi.fr>
2020-09-11 14:09:07 +0200

    test(core): add string matcher tests

--- test/fuzzy.test.js
+++ test/fuzzy.test.js
@@ -9,7 +9,41 @@ test("When creating a Fuzzy instance, items list should be accessible", () => {
     "Donut jelly cake I love",
   ];
   const fuzzy = new Fuzzy(items);
+  
   expect(fuzzy.items).not.toBe(undefined);
   expect(fuzzy.items).not.toBe(null);
   expect(fuzzy.items).toBe(items);
+});
+
+test("Should return a match when given a list of strings and a matcher pattern", () => {
+  const items = [
+    "Dessert jelly pastry sweet jelly beans pie cheesecake croissant",
+    "Cheesecake pudding sesame snaps cupcake pastry gummies macaroon",
+    "Macaroon cheesecake jelly beans tart",
+    "Donut jelly cake I love",
+  ];
+  const fuzzy = new Fuzzy(items);
+
+  const matches = fuzzy.search("cupcake");
+
+  expect(matches.length).not.toBe(0);
+});
+
+test("Should return several matches", () => {
+  const items = [
+    "Dessert jelly pastry sweet jelly beans pie cheesecake croissant",
+    "Cheesecake pudding sesame snaps cupcake pastry gummies macaroon",
+    "Macaroon cheesecake jelly beans tart",
+    "Donut jelly cake I love",
+  ];
+  const fuzzy = new Fuzzy(items);
+
+  const matches = fuzzy.search("cheesecake");
+
+  expect(matches.length).toBeGreaterThan(1);
+  expect(matches).toContainEqual({
+    item: 'Dessert jelly pastry sweet jelly beans pie cheesecake croissant',
+    index: 0,
+    matchesAt: [ 43 ]
+  });
 });
