b381dc27a571a6319b69d04e95c305aa4897ac3b
ArthurD <arthur.delamare1@viacesi.fr>
2020-09-14 11:26:21 +0200

    refactor(core): clean unused function

--- src/recognizy.ts
+++ src/recognizy.ts
@@ -1,6 +1,5 @@
 import { Fuzzy, FuzzyOptions } from "./fuzzy";
 import { rabinkarp } from "./hash/rabinkarp";
 import jaccard from "./vectors/jaccard";
-import singleSearch from "./string/naive";
 
-export { Fuzzy, FuzzyOptions, rabinkarp, jaccard, singleSearch };
+export { Fuzzy, FuzzyOptions, rabinkarp, jaccard};
--- src/string/naive.ts
+++
@@ -1,8 +0,0 @@
-export default function singleSearch(value: string, list: string[]): string {
-  for (let i = 0; i < list.length; i++) {
-    if (list[i].includes(value)) {
-      return list[i];
-    }
-  }
-  return "";
-}
