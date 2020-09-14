85e155df0c5463f52b9da71d5d47740b99661079
ArthurD <arthur.delamare1@viacesi.fr>
2020-09-14 11:26:58 +0200

    refactor(core): clean unused test

--- test/naive.test.js
+++
@@ -1,7 +0,0 @@
-const recognizy = require("../dist/recognizy");
-
-test("find a match into list", () => {
-  const array = ["I like apples", "tomatoes are great", "pizzas are the best"];
-  const searched = "apples";
-  expect(recognizy.singleSearch(searched, array)).toBe("I like apples");
-});
