43b5ff5dbb1a2286d381084a2e41d7dec07f00f9
Unknown <arthur.delamare1@viacesi.fr>
2020-09-29 11:58:23 +0200

    feat(core): global export getProperty function

--- src/recognizy.ts
+++ src/recognizy.ts
@@ -1,4 +1,5 @@
 import { Fuzzy, FuzzyOptions } from "./fuzzy";
 import { rabinkarp } from "./hash/rabinkarp";
+import { getProperty } from "./utils";
 
-export { Fuzzy, FuzzyOptions, rabinkarp };
+export { Fuzzy, FuzzyOptions, rabinkarp, getProperty };
